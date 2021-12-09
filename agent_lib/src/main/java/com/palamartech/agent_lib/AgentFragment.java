12a75b0742d593971b75ce1bc323eb9a8645b275
Okan Dönmez <you@example.com>
2021-12-09 15:05:31 +0300

    Progress on web token

--- agent_lib/src/main/java/com/palamartech/agent_lib/AgentFragment.java
+++ agent_lib/src/main/java/com/palamartech/agent_lib/AgentFragment.java
@@ -20,12 +20,10 @@ public class AgentFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_PARAM_WEBTOKEN = "webToken";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String webToken;
     private TextView txtGreeting;
     private EditText edtMessageBox;
     private Button btnSend;
@@ -38,16 +36,14 @@ public class AgentFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param webToken Parameter 1.
      * @return A new instance of fragment AgentFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AgentFragment newInstance(String param1, String param2) {
+    public static AgentFragment newInstance(String webToken) {
         AgentFragment fragment = new AgentFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM_WEBTOKEN, webToken);
         fragment.setArguments(args);
         return fragment;
     }
@@ -56,8 +52,7 @@ public class AgentFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            webToken = getArguments().getString(ARG_PARAM_WEBTOKEN);
         }
 
     }
@@ -74,7 +69,7 @@ public class AgentFragment extends Fragment {
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                txtGreeting.setText(edtMessageBox.getText().toString());
+                txtGreeting.setText(webToken);
                 edtMessageBox.setText("");
             }
         });
